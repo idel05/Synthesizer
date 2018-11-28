500536e7f383206db9de2a4180bab358b45e0f0f
idel05 <42978812+idel05@users.noreply.github.com>
2018-11-28 13:10:44 -0600

    who ate purple people.

--- app/src/main/java/com/example/idel2101/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/idel2101/synthesizer/SynthesizerActivity.java
@@ -2,12 +2,17 @@ package com.example.idel2101.synthesizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
+    private static final String TAG =
+            SynthesizerActivity.class.getName();
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
-    }
-}
+
+    }}
